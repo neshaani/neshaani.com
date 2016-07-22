00afb091067e83b914f720faa38329e21f6be684
Behnam Baharmand <behnam@baharmand.com>
2016-07-22 13:13:59 +0430

    Update ui and add initial functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
-var logger = require('./LoggerModule')
+var logger = require('./LoggerModule');
 
-logger.log('Welcome!');
+logger.log('Project Home: https://github.com/neshaani/neshaani');
+
+(function($) {
+	$('input#state').on('click', function() {
+		var req = {
+			"url" : "http://google.com"
+		};
+
+		$.ajax({
+			type: "POST",
+			url: "https://api.neshaani.com/api/generate",
+			data: JSON.stringify(req),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(data){
+				alert(data);
+			},
+			failure: function(errMsg) {
+				alert(errMsg);
+			}
+		});
+
+		$('p.result').fadeIn();
+
+	});
+})(jQuery);
